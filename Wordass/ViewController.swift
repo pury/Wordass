77eef19a2908fc71dfb3c90cbf59a5d7281016b7
Pury <szwzjx@163.com>
2017-03-07 18:08:44 +0800

    [update]: base view

--- Wordass/ViewController.swift
+++ Wordass/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // http://cn.bing.com/dict/search?q=word&FORM=BDVSP2&qpvt=word
+        //let url = NSURL(string: "http://fanyi.baidu.com/?aldtype=85#en/zh/word");
+        //let url = NSURL(string: "http://cn.bing.com/dict/search?q=word&FORM=BDVSP2&qpvt=word");
+        
+        //let request = NSURLRequest(url: url as! URL);
+        //webview2.loadRequest(request as URLRequest)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
